64cd9ee6d0d6182b5ba589e3e32b29e5714dff93
Julieta <jltmtzc@gmail.com>
2017-07-28 12:35:21 -0700

    fix bug in run_client
    `jobid` can be an array and have brackets

--- run_client.py
+++ run_client.py
@@ -11,9 +11,9 @@ args = parser.parse_args()
 if args.request and args.release:
   raise(ValueError, "both request and release can't be asked at the same time")
 
-# Ada passes something like '5580.ada.cs.ubc.ca'
+# Ada passes something like '5580.ada.cs.ubc.ca' or '5580[1].ada.cs.ubc.ca'
 jid = args.jobid.split('.')[0]
-locker_cli = LockerClient( int(jid) )
+locker_cli = LockerClient( jid )
 
 if args.request:
   print "export CUDA_VISIBLE_DEVICES={0}".format( locker_cli.request_gpu() )
